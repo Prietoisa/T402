22e3160cc4bf18dd8875eac0b0c16ed1cf6fc6b7
unknown <mreginadasilva@yahoo.com.br>
2019-07-25 16:56:44 -0300

    criar formularios e salvar na base

--- ideias_malucas/website/models.py
+++ ideias_malucas/website/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    GENEROS = (
+        ('F','Femenino'),
+        ('M','Masculino'),
+        ('O', 'Outro'),
+    )
+    
+    nome = models.CharField(
+        max_length= 225,
+        verbose_name= 'Nome'
+    )
+
+    sobrenome = models.CharField(
+    max_length=225,
+    verbose_name='Sobrenome'
+    )
+
+    generos = models.CharField(
+    max_length=225,
+    verbose_name='GENERO',
+    choices=GENEROS
+    )  
+
+    email = models.EmailField(
+    max_length=225,
+    verbose_name='E-mail'
+    )
+
+    data_de_criacao = models.DateTimeField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome+ ' ' + self.sobrenome
